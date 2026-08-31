f68de11fbda73ae5b94229dd0f079a961827d15e
kodiyak <mathewsto51@gmail.com>
2026-08-31 15:47:12 -0300

    feat: integrate company details into order document using APP_CONFIG

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,13 @@
 export const APP_CONFIG = {
   name: "Aura",
   description: "Gestão para Ateliê de Costura",
+  company: {
+    name: "Paty",
+    email: "patricia-teodoro_@hotmail.com",
+    phone: "+55 (11) 9 5930-2924",
+  },
   pdf: {
     filenamePrefix: "paty-pedido",
   },
-};
+} as const;
+export type AppConfig = typeof APP_CONFIG;
--- src/app/orders/[orderId]/_components/order-document.tsx
+++ src/app/orders/[orderId]/_components/order-document.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   View,
 } from "@react-pdf/renderer";
+import { APP_CONFIG } from "@/app.config";
 import Logo from "@/assets/logo_pdf.png";
 import type { Api } from "@/lib/clients/api/types";
 import { useCurrencyFormatter } from "@/lib/hooks";
@@ -15,37 +16,16 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     backgroundColor: "#ffffff",
   },
-  header: {
-    padding: 10,
-    backgroundColor: "#f5f5f5",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+  logo: {
+    width: 200,
+    height: 100,
+    objectFit: "contain",
   },
   card: {
     padding: 30,
   },
-  logo: {
-    width: 200,
-    height: 200,
-    margin: "20px auto",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 18,
-    textAlign: "center",
-    color: "#666666",
-    fontWeight: "bold",
-    marginBottom: 30,
+  wFull: {
+    width: "100%",
   },
   row: {
     flexDirection: "row",
@@ -61,10 +41,32 @@ const styles = StyleSheet.create({
   textLg: {
     fontSize: 16,
   },
+  textMd: {
+    fontSize: 14,
+  },
   textSm: {
     fontSize: 12,
+  },
+  textXs: {
+    fontSize: 10,
+  },
+  textXxs: {
+    fontSize: 10,
+  },
+  textMuted: {
     color: "#888888",
+  },
+  textBold: {
     fontWeight: "bold",
+  },
+  borderB: {
+    borderBottom: 1,
+  },
+  borderT: {
+    borderTop: 1,
+  },
+  bordered: {
+    borderColor: "#ccc",
   },
 });
 
@@ -83,39 +85,71 @@ export default function OrderDocument({ order }: OrderDocumentProps) {
   return (
     <Document key={Date.now().toString()}>
       <Page size="A4" style={styles.page}>
-        <View style={styles.header}>
+        <View style={styles.row}>
           <Image source={Logo.src} style={styles.logo} />
+          <View
+            style={[
+              styles.col,
+              { alignItems: "flex-end", padding: 10, flex: 1 },
+            ]}
+          >
+            <Text style={styles.textXxs}>{APP_CONFIG.company.name}</Text>
+            <Text style={styles.textXxs}>{APP_CONFIG.company.email}</Text>
+            <Text style={styles.textXxs}>{APP_CONFIG.company.phone}</Text>
+            <Text style={[styles.textXxs, { marginTop: 10 }]}>
+              Data de Emissão: {new Date().toLocaleString()}
+            </Text>
+          </View>
         </View>
-        <View style={styles.section}>
-          <View style={styles.card}>
-            <Text style={styles.title}>Pedido {`#${order.orderNumber}`}</Text>
-            <Text style={styles.subtitle}>{order.customer.name}</Text>
-            {order.items
-              .filter((i) => i.quantity > 0)
-              .map((item) => (
-                <View key={item.id} style={{ ...styles.row, marginBottom: 10 }}>
-                  <View style={styles.col}>
-                    <Text style={styles.textLg}>
-                      {item.quantity} x {item.name}
-                    </Text>
-                    <Text style={styles.textSm}>
-                      {formatCurrency(item.price)} / unidade
-                    </Text>
-                  </View>
-                  <Text style={{ fontWeight: "bold" }}>
-                    {formatCurrency(item.price * item.quantity)}
-                  </Text>
-                </View>
-              ))}
-            <View style={{ ...styles.row, marginTop: 20 }}>
-              <View />
-              <View style={{ ...styles.col, alignItems: "flex-end" }}>
-                <Text style={{ ...styles.textLg, fontWeight: "bold" }}>
-                  Total
+        <View
+          style={{
+            ...styles.col,
+            borderTop: 1,
+            borderBottom: 1,
+            borderColor: "#ccc",
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+          }}
+        >
+          <Text style={[styles.textXl, styles.textBold]}>
+            Pedido {`#${order.orderNumber}`}
+          </Text>
+          <Text style={[styles.textSm, styles.textMuted]}>
+            {order.customer.name}
+          </Text>
+        </View>
+        <View style={styles.wFull}>
+          {order.items.map((item) => (
+            <View
+              key={item.id}
+              style={[
+                styles.row,
+                styles.borderB,
+                styles.bordered,
+                { paddingHorizontal: 10, paddingVertical: 5 },
+              ]}
+            >
+              <View style={[styles.col, { flex: 1 }]}>
+                <Text style={[styles.textSm, styles.textBold]}>
+                  {item.quantity} x {item.name}
                 </Text>
-                <Text style={styles.textXl}>{formatCurrency(amountTotal)}</Text>
+                <Text style={[styles.textXxs, styles.textMuted]}>
+                  {formatCurrency(item.price)} / unidade
+                </Text>
               </View>
+              <Text style={[styles.textSm, styles.textBold]}>
+                {formatCurrency(item.price * item.quantity)}
+              </Text>
             </View>
+          ))}
+        </View>
+        <View style={styles.row}>
+          <View style={{ flex: 1 }} />
+          <View style={{ ...styles.col, padding: 10, alignItems: "flex-end" }}>
+            <Text style={[styles.textMd, styles.textMuted, styles.textBold]}>
+              Total
+            </Text>
+            <Text style={[styles.textXl]}>{formatCurrency(amountTotal)}</Text>
           </View>
         </View>
       </Page>
