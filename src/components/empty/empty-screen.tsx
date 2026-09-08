b02cf8d2a67738aaece4a19466b3c9f1cc84102e
kodiyak <mathewsto51@gmail.com>
2026-09-08 15:42:22 -0300

    feat: simplify EmptyScreen component styling; remove unnecessary borders and background

--- src/components/empty/empty-screen.tsx
+++ src/components/empty/empty-screen.tsx
@@ -13,7 +13,7 @@ export default function EmptyScreen({
   icon,
 }: EmptyScreenProps) {
   return (
-    <div className="relative border border-dashed flex flex-col rounded-2xl dark:bg-muted/20">
+    <div className="relative">
       <div className="flex-1 flex flex-col items-center justify-center pt-6">
         <div className="size-24 rounded-full border flex items-center justify-center bg-background">
           {icon ?? <ListTreeIcon className="size-8 text-muted-foreground" />}
@@ -22,7 +22,7 @@ export default function EmptyScreen({
           <span className="text-xl font-mono font-bold my-3">{title}</span>
         )}
       </div>
-      <div className="border-t border-dashed px-12 py-2 dark:bg-muted/30 rounded-b-2xl">
+      <div className="px-12 py-2">
         <p className="text-center text-sm text-muted-foreground">
           {description}
         </p>
